0c1c6d7e68d674f35d6660d4b7039d63ebe2451a
EwanShilland <e.shilland@ucl.ac.uk>
2023-01-18 12:09:16 +0000

    added another test line

--- day3gitLinkedNHMpractise.R
+++ day3gitLinkedNHMpractise.R
@@ -1,3 +1,6 @@
 #18/01/23
 #2ns r project - git linked one
-#created new pro
+#created new repo on website called frog-riddler
+#go to code on repo and copy link
+# then make new r project and use the repository version and choose git
+#make edits then commit to update
