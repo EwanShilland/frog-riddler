39b80fa5f1c36421133690ba9d6f2f9374e3514d
EwanShilland <e.shilland@ucl.ac.uk>
2023-01-18 12:14:56 +0000

    more notes on git/r settings

--- day3gitLinkedNHMpractise.R
+++ day3gitLinkedNHMpractise.R
@@ -4,3 +4,6 @@
 #go to code on repo and copy link
 # then make new r project and use the repository version and choose git
 #make edits then commit to update
+#can go to diff revert to go back one step#carry on saving as normal
+#need to add file to get git to watch it
+#check tickboxes in git tab to commit changes for each file you're wirking on
